088b7e7ad6d0c1c7e733ecb252dba927b0ff654a
Viacheslav Nykonenko <v.nykonenko@gmail.com>
2025-12-15 18:10:18 +0000

    implemented PostPreviewSerializer, fixed hashtag adding in PostSerializer.create method

--- social_media/serializers.py
+++ social_media/serializers.py
@@ -1,7 +1,7 @@
 from django.db import transaction
 from rest_framework import serializers
 
-from social_media.models import Post, Hashtag
+from social_media.models import Post, Hashtag, Comment
 
 
 class HashtagSerializer(serializers.ModelSerializer):
@@ -12,20 +12,37 @@ class HashtagSerializer(serializers.ModelSerializer):
 
 class PostSerializer(serializers.ModelSerializer):
     author = serializers.StringRelatedField(many=False, read_only=True)
-    hashtags = HashtagSerializer(many=True, read_only=False)
+    hashtags = HashtagSerializer(many=True, read_only=False, required=False)
 
     class Meta:
         model = Post
-        fields = ("id", "author", "content", "media_files", "hashtags")
+        fields = ("id", "author", "content", "media_files", "shared_post", "hashtags")
 
     def create(self, validated_data):
         with transaction.atomic():
             hashtags = validated_data.pop("hashtags")
             post = Post.objects.create(**validated_data)
             for hashtag in hashtags:
-                hashtag = Hashtag.objects.create(**hashtag)
+                hashtag = Hashtag.objects.get_or_create(**hashtag)
                 post.hashtags.add(hashtag)
             return post
+
+
+class PostPreviewSerializer(PostSerializer):
+    author = serializers.StringRelatedField(many=False, read_only=True)
+    content_preview = serializers.SerializerMethodField(read_only=True)
+
+    class Meta(PostSerializer.Meta):
+        fields = ("author", "content_preview")
+
+    @staticmethod
+    def get_content_preview(post):
+        output = {}
+        if post.content:
+            output["content"] = post.content
+        if post.media_files:
+            output["media_files"] = post.media_files[:250]
+        return output
 
 
 class PostListSerializer(PostSerializer):
@@ -33,13 +50,14 @@ class PostListSerializer(PostSerializer):
     shares = serializers.IntegerField(read_only=True)
     comments_num = serializers.IntegerField(read_only=True)
     hashtags = serializers.StringRelatedField(many=True, read_only=True)
+    shared_post = PostPreviewSerializer(many=False, read_only=True)
 
     class Meta:
         model = Post
         fields = (
             "author",
-            "content_preview",
             "media_files",
+            "shared_post",
             "hashtags",
             "likes",
             "shares",
@@ -47,7 +65,18 @@ class PostListSerializer(PostSerializer):
         )
 
 
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField(many=False, read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ("id", "author", "content", "media_files", "created_at")
+
+
 class PostRetrieveSerializer(PostListSerializer):
+    liked_by = serializers.StringRelatedField(many=True, read_only=True)
+    shared_by = serializers.StringRelatedField(many=True, read_only=True)
+    comments = CommentSerializer(many=True, read_only=True)
 
     class Meta:
         model = Post
@@ -58,7 +87,9 @@ class PostRetrieveSerializer(PostListSerializer):
             "media_files",
             "hashtags",
             "likes",
+            "liked_by",
             "shares",
+            "shared_by",
             "comments_num",
             "comments",
         )
